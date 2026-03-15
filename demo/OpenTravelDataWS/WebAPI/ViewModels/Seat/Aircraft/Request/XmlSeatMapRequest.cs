ed80ddbf956109cc5848c864d8548909ba95b2b2
a-chhiong <a-chhiong@outlook.com>
2026-03-15 09:31:25 +0800

    [chore] flatten folder

--- demo/OpenTravelDataWS/WebAPI/ViewModels/Seat/Aircraft/Request/XmlSeatMapRequest.cs
+++
@@ -1,9 +0,0 @@
-namespace WebAPI.ViewModels.Seat.Aircraft.Request;
-
-public class XmlSeatMapRequest
-{
-    /// <summary>
-    /// 機型代號
-    /// </summary>
-    public string? ACVCode { get; set; }
-}
